483d00d85f80b6c1cc5c0fe30ae1233a7082ae40
Celso Henrique Taliatelli <celsotaliatelli@hotmail.com>
2017-05-06 14:25:02 -0300

    Padrão MVVM

---
+++ TesteDrive/TesteDrive/ViewModels/AgendamentoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TesteDrive.Models;
+
+namespace TesteDrive.ViewModels
+{
+   
+    public class AgendamentoViewModel
+    {
+        public Agendamento Agendamento { get; set; }
+        public Veiculo Veiculo
+        {
+            get
+            {
+                return Agendamento.Veiculo;
+            }
+            set
+            {
+                Agendamento.Veiculo = value;
+            }
+        }
+        public string Nome { get { return Agendamento.Nome; } set { Agendamento.Nome = value; } }
+        public string Fone { get; set; }
+        public string Email { get; set; }
+
+
+        public DateTime DataAgendamento
+        {
+            get
+            {
+                return Agendamento.DataAgendamento;
+            }
+            set
+            {
+                Agendamento.DataAgendamento = value;
+            }
+        }
+        public TimeSpan HoraAgendamento
+        {
+            get
+            {
+                return Agendamento.HoraAgendamento;
+            }
+            set
+            {
+                Agendamento.HoraAgendamento = value;
+            }
+        }
+
+        public AgendamentoViewModel(Veiculo veiculo)
+        {
+            this.Agendamento = new Agendamento();
+            this.Agendamento.Veiculo = Veiculo;
+        }
+    }
+}
--- TesteDrive/TesteDrive/Views/AgendaView.xaml.cs
+++ TesteDrive/TesteDrive/Views/AgendaView.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TesteDrive.Models;
+using TesteDrive.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,57 +13,18 @@ namespace TesteDrive.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AgendaView : ContentPage
 	{
-        public Agendamento Agendamento { get; set; }
-        public Veiculo Veiculo
-        {
-            get
-            {
-                return Agendamento.Veiculo;
-            }
-            set
-            {
-                Agendamento.Veiculo = value;
-            }
-        }
-        public string Nome { get { return Agendamento.Nome; } set { Agendamento.Nome = value; } }
-        public string Fone { get; set; }
-        public string Email { get; set; }
-
-        
-        public DateTime DataAgendamento
-        {
-            get
-            {
-                return Agendamento.DataAgendamento;
-            }
-            set
-            {
-                Agendamento.DataAgendamento = value;
-            }
-        }
-        public TimeSpan HoraAgendamento {
-            get
-            {
-                return Agendamento.HoraAgendamento;
-            }
-            set
-            {
-                Agendamento.HoraAgendamento = value;
-            }
-        }
-
+        public AgendamentoViewModel viewModel { get; set; }
 
         public AgendaView (Veiculo veiculo)
 		{
 			InitializeComponent ();
-            this.Agendamento = new Agendamento();
-            this.Agendamento.Veiculo = Veiculo;
-            this.BindingContext = this;
+            this.viewModel = new AgendamentoViewModel(veiculo);
+            this.BindingContext = this.viewModel;
 		}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Agendamento", "Nome:" + Nome, "Ok");
+            DisplayAlert("Agendamento", "Nome:" + viewModel.Agendamento.Nome, "Ok");
         }
     }
 }
